d92e8685b0bcee6f616481cab633f462b2bcaee0
Mazzucca Kristian <mazzuccakristian@gmail.com>
2022-07-07 19:33:18 +0200

    Major refactoring. Prototype.

--- main.h
+++ main.h
@@ -1,29 +1,11 @@
 #ifndef MAIN_H
 #define MAIN_H
+
 #include <iostream>
+#include "mysql_connection.h"
+#include "Database/DB_Handler.h"
 
 void ShowLoginMenu();
 int GetLoginChoice();
-
-void ShowLoginMenu(){
-    std::cout << "--- LOGIN ---" << std::endl;
-    std::cout << "1. User;" << std::endl;
-    std::cout << "2. Technician;" << std::endl;
-    std::cout << "0. Exit." << std::endl; 
-}
-
-int GetLoginChoice(){
-    int choice{0};
-    bool isValid{true};
-    do{
-    std::cout << "Enter your option: ";
-    std::cin >> choice;
-    if(choice < 0 || choice > 2){
-        std::cout << "Option not allowed. Please, try again..." << std::endl;
-        isValid = false;
-    }
-    }while(!isValid);
-    return choice;
-}
 
 #endif
