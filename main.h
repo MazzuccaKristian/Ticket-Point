08e3e5543ec740494ce89fb17e5b8407d9d60fa6
Mazzucca Kristian <mazzuccakristian@gmail.com>
2022-07-18 19:24:01 +0200

    CollectUserData defined. Working.

--- main.h
+++ main.h
@@ -2,10 +2,12 @@
 #define MAIN_H
 
 #include <iostream>
+#include <string>
 #include "mysql_connection.h"
 #include "Database/DB_Handler.h"
 
 void ShowLoginMenu();
 int GetLoginChoice();
+void CollectUserData(std::string *dataCollection);
 
 #endif
