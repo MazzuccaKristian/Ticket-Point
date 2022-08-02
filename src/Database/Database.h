5903b97e38b29acbb312d0af8f307c8bbe9e1f82
Mazzucca Kristian <mazzuccakristian@gmail.com>
2022-08-02 18:16:52 +0200

    DatabaseSetup and TryLogin added.

---
+++ src/Database/Database.h
@@ -0,0 +1,22 @@
+#ifndef DATABASE_H
+#define DATABASE_H
+
+#include "mysql_connection.h"
+#include <cppconn/driver.h>
+#include <cppconn/resultset.h>
+#include <cppconn/exception.h>
+#include <cppconn/statement.h>
+#include <cppconn/prepared_statement.h>
+#include <string>
+
+const std::string DB_HOST {"tcp://127.0.0.1:3306"};
+const std::string DB_USER {"ticketpoint_user"};
+const std::string DB_PASSWORD {"T!cketPointP4ssword"};
+const std::string DB_SCHEMA {"TicketPoint_DB"};
+
+const std::string loginQuery {"SELECT * FROM User WHERE Name = ? AND Password = ?"};
+
+sql::Connection *DatabaseSetup();
+std::string TryLogin(sql::Connection *connection, const std::string loginRecord);
+
+#endif
