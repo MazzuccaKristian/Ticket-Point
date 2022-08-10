e48225832884c6e832b825a016b30f2b0fd9e236
Mazzucca Kristian <mazzuccakristian@gmail.com>
2022-08-10 16:24:18 +0200

    User's login fixed. Custom tuple for user added.

--- src/Utils/Utils.cpp
+++ src/Utils/Utils.cpp
@@ -38,3 +38,52 @@ std::string CollectUserData(){
     loginDataRecord = loginDataRecord + ":" + loginDataRecord_temp;
     return loginDataRecord;
 }
+
+/**
+ * @brief Tokenize login result (as std::string) and create a tuple.
+ * 
+ * @param loginResultString 
+ * @return std::tuple<int userId, std::string mail, std::string name, std::string lastname, std::string password> 
+ */
+std::tuple<int, std::string, std::string, std::string, std::string> BuildLoginResultTuple(std::string loginResultString){
+    std::size_t startDelimiter {0};
+    std::size_t endDelimiter {loginResultString.find_first_of(':')};
+    int userId {std::stoi(loginResultString.substr(startDelimiter, endDelimiter - startDelimiter))};
+    startDelimiter = endDelimiter + 1;
+    endDelimiter = loginResultString.find_first_of(':', startDelimiter);
+    std::string mail {loginResultString.substr(startDelimiter, endDelimiter - startDelimiter)};
+    startDelimiter = endDelimiter + 1;
+    endDelimiter = loginResultString.find_first_of(':', startDelimiter);
+    std::string name {loginResultString.substr(startDelimiter, endDelimiter - startDelimiter)};
+    startDelimiter = endDelimiter + 1;
+    endDelimiter = loginResultString.find_first_of(':', startDelimiter);
+    std::string lastname {loginResultString.substr(startDelimiter, endDelimiter - startDelimiter)};
+    startDelimiter = endDelimiter + 1;
+    endDelimiter = loginResultString.find_first_of(':', startDelimiter);
+    std::string password {loginResultString.substr(startDelimiter, endDelimiter - startDelimiter)};
+    return std::make_tuple(userId, mail, name, lastname, password);
+}
+
+/**
+ * @brief Collect data and attemp login. Then, return a tuple with user's info.
+ * 
+ * @param connection 
+ * @return std::tuple<int, std::string, std::string, std::string, std::string> 
+ */
+std::tuple<int, std::string, std::string, std::string, std::string> LoginPhase(sql::Connection *connection){
+    std::string loginData {""};
+    std::string loggedUserData {""};
+    bool isLoginValid {false};
+    do{
+        loginData = CollectUserData();
+        loggedUserData = TryLogin(connection, loginData);
+        if(loggedUserData.compare("-1") != 0){
+            isLoginValid = true;
+        }else{
+            std::cout << "Login failed. Please, try again..." << std::endl;
+        }
+    }while(!isLoginValid);
+    std::tuple<int, std::string, std::string, std::string, std::string> loginResultTuple;
+    loginResultTuple = BuildLoginResultTuple(loggedUserData);
+    return loginResultTuple;
+}
