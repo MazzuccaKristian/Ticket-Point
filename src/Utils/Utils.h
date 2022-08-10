82b552f358e5b04329d6727b78e96dea5226a2be
Mazzucca Kristian <mazzuccakristian@gmail.com>
2022-08-10 16:23:44 +0200

    User's info and login handled with custom tuple.

--- src/Utils/Utils.h
+++ src/Utils/Utils.h
@@ -2,9 +2,13 @@
 #define UTILS_H
 
 #include <iostream>
+#include "mysql_connection.h"
+#include "../Database/Database.h"
 
 void ShowMainMenu();
 int GetLoginChoice();
 std::string CollectUserData();
+std::tuple<int, std::string, std::string, std::string, std::string> BuildLoginResultTuple(std::string loginResultString);
+std::tuple<int, std::string, std::string, std::string, std::string> LoginPhase(sql::Connection *connection);
 
 #endif
