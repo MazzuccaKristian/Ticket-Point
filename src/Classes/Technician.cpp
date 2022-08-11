65315bf3e407efb52ba69d65afeaa6bad8c96f97
Mazzucca Kristian <mazzuccakristian@gmail.com>
2022-08-11 15:39:57 +0200

    Class' attributes added. Constructor updated.

--- src/Classes/Technician.cpp
+++ src/Classes/Technician.cpp
@@ -1,5 +1,5 @@
 #include "Technician.h"
 
-Technician::Technician(int id, std::string mail, std::string name, std::string lastname, std::string password) : Person(id, mail, name, lastname, password){
-    
+Technician::Technician(int id, std::string mail, std::string name, std::string lastname, std::string password, int skillId) : Person(id, mail, name, lastname, password){
+    Technician_skillId = skillId;
 }
--- src/Classes/Technician.h
+++ src/Classes/Technician.h
@@ -4,8 +4,11 @@
 #include "Person.h"
 
 class Technician : public Person{
+    private:
+        int Technician_skillId;
+
     public:
-        Technician(int id, std::string mail, std::string name, std::string lastname, std::string password);
+        Technician(int id, std::string mail, std::string name, std::string lastname, std::string password, int skillId);
 };
 
 #endif
