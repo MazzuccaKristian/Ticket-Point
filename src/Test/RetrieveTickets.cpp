99cf7a49e59e436cbce72912d7728980e15477c1
Mazzucca Kristian <mazzuccakristian@gmail.com>
2022-08-24 18:06:28 +0200

    Test fixed. Working.

--- src/Test/RetrieveTickets.cpp
+++ src/Test/RetrieveTickets.cpp
@@ -4,7 +4,7 @@
 int main(){
     sql::Connection *connection {DatabaseSetup()};
 
-    sql::ResultSet *result {RetrieveOpenTickets(connection, 1)};
+    sql::ResultSet *result {RetrieveOpenTickets(connection, 0)};
     if(result -> next()){
         // Here if ResultSet contains - at least - one element.
         return 0;
